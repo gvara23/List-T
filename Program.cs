9c460cb4f79a2d3a2bfd24f9854b3bea31380857
Heidy <heidyguevara23@hotmail.com>
2024-04-17 17:22:53 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace List_T
+{
+    class Program
+    {
+        static void Main()
+        {
+
+            List<string> stringList = new List<string>();
+
+            stringList.Add("Première chaîne");
+            stringList.Add("Deuxième chaîne");
+            stringList.Add("Troisième chaîne");
+
+            stringList.RemoveAt(0);
+
+            stringList.Remove("Troisième chaîne");
+
+            stringList[0] = "Chaîne modifiée";
+
+            Console.WriteLine("Contenu de la liste après manipulation :");
+            foreach (string str in stringList)
+            {
+                Console.WriteLine(str);
+            }
+        }
+    }
+}
